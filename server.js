cc5699cb96474d0a4aae487fbd7cf2c1c80fe864
pr1438 <philip.rodrigues@solnet.co.nz>
2017-09-12 17:35:43 +1200

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('<H1>Hello World!</H1>')
+    res.send('<H1>Hello World!</H1><p>We are serial!</p>')
 })
 
 app.listen(3000, function () {
